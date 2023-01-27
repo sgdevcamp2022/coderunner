66b4ebd06d4dee79888f9fe3eb97a075431144dd
parkgain <pgi082247@gmail.com>
2023-01-27 12:39:24 +0900

    feat : 채널 생성 조회 api 만듦

--- channel-service/controllers/channel-controller.js
+++ channel-service/controllers/channel-controller.js
@@ -2,27 +2,24 @@ const db = require("../db/connection");
 const query = require("../db/channel-query");
 
 module.exports.getChannelList = async (req, res) => {
-  try {
-    db.query(query.getChannelList);
-  } catch {
-    return res.status(500).json(err);
-  }
+  db.query(query.getChannelList, (error, rows) => {
+    if (error) throw error;
+    res.send(rows);
+  });
 };
 module.exports.getChannelInfo = async (req, res) => {
   const id = req.params.id;
-  try {
-    const rows = db.query(query.getChannelInfo(id));
-    return res.json(rows[0]);
-  } catch {
-    return res.status(500).json(err);
-  }
+
+  db.query(query.getChannelInfo(id), (error, rows) => {
+    if (error) throw error;
+    res.send(rows);
+  });
 };
 module.exports.createChannel = async (req, res) => {
   const { name, outline, private } = req.body;
-  try {
-    db.query(query.createChannel(name, outline, private));
-    return res.json({ message: "created!" });
-  } catch {
-    return res.status(500).json(err);
-  }
+
+  db.query(query.createChannel(name, outline, private), (error, rows) => {
+    if (error) throw error;
+    res.send(rows);
+  });
 };
--- channel-service/db/channel-query.js
+++ channel-service/db/channel-query.js
@@ -1,6 +1,6 @@
 const getChannelList = "select name from channel";
 const getChannelInfo = (id) => {
-  return `select * from channel where channel_id is ${id}`;
+  return `select * from channel where channel_id=${id}`;
 };
 const createChannel = (name, outline, private) => {
   return `insert into channel (name,outline,private) values (${name},${outline},${private})`;
