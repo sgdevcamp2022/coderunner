62d191e984a72d8aa5adb40c1b693282f2902685
parkgain <pgi082247@gmail.com>
2023-02-09 16:16:35 +0900

    docs : 채널 사용자 목록 불러오는 api 쿼리문 수정

--- channel-service/db/channel-query.js
+++ channel-service/db/channel-query.js
@@ -6,7 +6,9 @@ const createChannel = (name) => {
   return `insert into channel (name) values ("${name}")`;
 };
 const getUserList = (id) => {
-  return `select user_id from user_channel where channel_id=${id}`;
+  return `select nickname
+  from user join user_channel
+  on user.user_id = user_channel.user_id WHERE user.user_id= ${id}`;
 };
 const addUser = (user_id, id) => {
   return `insert into user_channel (user_id,channel_id) values (${user_id}, ${id}) `;
