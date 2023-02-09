287db856a48f761b0e1dbc7fbb43ad1328616bfa
parkgain <pgi082247@gmail.com>
2023-02-09 17:10:25 +0900

    docs: getUserList 쿼리문 소문자로 통일

--- channel-service/db/channel-query.js
+++ channel-service/db/channel-query.js
@@ -6,9 +6,7 @@ const createChannel = (name) => {
   return `insert into channel (name) values ("${name}")`;
 };
 const getUserList = (id) => {
-  return `select nickname
-  from user join user_channel
-  on user.user_id = user_channel.user_id WHERE user.user_id= ${id}`;
+  return `select nickname from user join user_channel on user.user_id = user_channel.user_id where user.user_id= ${id}`;
 };
 const addUser = (user_id, id) => {
   return `insert into user_channel (user_id,channel_id) values (${user_id}, ${id}) `;
