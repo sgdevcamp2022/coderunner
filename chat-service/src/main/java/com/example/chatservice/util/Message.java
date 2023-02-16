e8fabcc4e86128ac723a898409612580b930090d
장소명 <som0ng@naver.com>
2023-02-16 10:13:02 +0900

    feat : 다른 메시지 추가

--- chat-service/src/main/java/com/example/chatservice/util/Message.java
+++ chat-service/src/main/java/com/example/chatservice/util/Message.java
@@ -9,5 +9,9 @@ public class Message {
     public static final String MESSAGE_FIND_SUCCESS_MESSAGE = "메시지 조회 성공";
     public static final String MESSAGE_FIND_FAIL_MESSAGE = "메시지 조회 실패";
 
+    public static final String BAD_REQUEST = "잘못된 요청 주소 입니다.";
+    public static final String PARAMETER_TYPE_MISMATCH = "잘못된 인자값 입니다.";
+    public static final String NOT_FOUND = "인자값이 존재하지 않습니다.";
+
 
 }
