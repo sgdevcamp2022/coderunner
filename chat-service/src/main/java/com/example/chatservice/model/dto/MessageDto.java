3d5ef57474135252fb2b7ee8b30b48efd56b8a4b
장소명 <som0ng@naver.com>
2023-02-19 18:43:19 +0900

    refactor : MessageDto 수정

--- chat-service/src/main/java/com/example/chatservice/model/dto/MessageDto.java
+++ chat-service/src/main/java/com/example/chatservice/model/dto/MessageDto.java
@@ -2,6 +2,10 @@ package com.example.chatservice.model.dto;
 
 import com.example.chatservice.model.dto.ChatPostDto.ChatPostReq;
 import com.example.chatservice.model.entity.ChatPost;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,8 +20,13 @@ import java.time.LocalDateTime;
 public class MessageDto {
 
     private int roomId;
+
     private String nickname;
+
     private String content;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createTime;
 
     public static MessageDto toEntity(ChatPostReq post) {
